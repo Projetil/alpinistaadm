10fadd5b23a0e9d55fa32d955256b2b7a3151670
Claudevanio <projetil.tech@outlook.com>
2025-01-08 15:19:54 -0300

    fix: build error

--- src/app/(adm)/home/[id]/components/Tab1Modal.tsx
+++ src/app/(adm)/home/[id]/components/Tab1Modal.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { IRisk, riskSeverity, riskStatus } from "@/types/IRisk";
 import { formatDateToDDMMYYYY } from "@/utils/formatString";
 import { IoInformationCircle } from "react-icons/io5";
@@ -9,16 +10,7 @@ const Tab1Modal = ({
   currentRisk?: IRisk;
   nameResponsible?: string;
 }) => {
-const Tab1Modal = ({
-  currentRisk,
-  nameResponsible,
-}: {
-  currentRisk?: IRisk;
-  nameResponsible?: string;
-}) => {
   return (
-    <div className="bg-[#FBFBFB] p-3 rounded-lg overflow-y-auto">
-      <div className="flex w-full justify-between mb-8 ">
     <div className="bg-[#FBFBFB] p-3 rounded-lg overflow-y-auto">
       <div className="flex w-full justify-between mb-8 ">
         <h4 className="font-semibold text-[#050506]">Informações principais</h4>
@@ -66,21 +58,17 @@ const Tab1Modal = ({
               {" "}
               {currentRisk && currentRisk.updatedAt
                 ? formatDateToDDMMYYYY(currentRisk.updatedAt)
-              {currentRisk && currentRisk.updatedAt
-                ? formatDateToDDMMYYYY(currentRisk.updatedAt)
                 : ""}
             </p>
           </div>
         </div>
       </div>
       <div className="flex flex-col gap-2 mt-3 overflow-auto">
-      <div className="flex flex-col gap-2 mt-3 overflow-auto">
         <p className="font-semibold text-[#40414A]">Descrição</p>
         <p
           dangerouslySetInnerHTML={{
             __html: currentRisk ? currentRisk.description : "",
           }}
-          className="text-[#80828D] overflow-auto break-words"
           className="text-[#80828D] overflow-auto break-words"
         ></p>
       </div>
